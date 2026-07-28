fbf777c8a9e456b321bcc932fa6954a0eb9dfbdf
izzy9118-blip <izzy_9118@hotmail.com>
2026-07-28 05:57:49 -0500

    Update CORPUS-WIT-102 regression for fifth TP witness

--- tests/test_corpus_wit_102_platform_registration.py
+++ tests/test_corpus_wit_102_platform_registration.py
@@ -69,7 +69,7 @@ class CorpusWitness102PlatformRegistrationTests(unittest.TestCase):
         )
         self.assertEqual(
             corpus["termination"]["theologico_political_reviewed_witness_state"],
-            "INCOMPLETE_4_OF_19",
+            "INCOMPLETE_5_OF_19",
         )
         self.assertEqual(
             corpus["termination"]["theologico_political_independent_study_state"],
